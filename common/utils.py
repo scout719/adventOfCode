89cb24f564d614b3bed3b0784b2d9e4f00038bfa
scout719 <helder.marques1991@gmail.com>
2019-01-02 17:13:12 +0000

    Fix lint problems on Utils

--- common/utils.py
+++ common/utils.py
@@ -5,20 +5,21 @@ from timeit import default_timer as timer
 """ AUX FUNCTIONS """
 class SignalCatchingError(Exception):
     """ Base class for exceptions in this module. """
-    pass
 
 HEAVY_EXERCISE = "nil (too computationally heavy)"
-EXERCISE_TIMEOUT = 120 #secs
+EXERCISE_TIMEOUT = 4000000 #secs
 
 def timeout(seconds_before_timeout):
     def deco(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            res = [SignalCatchingError('function [%s] timeout [%s seconds] exceeded!' % (func.__name__, seconds_before_timeout))]
+
+            res = [SignalCatchingError('function [%s] timeout [%s seconds] exceeded!' % \
+                                       (func.__name__, seconds_before_timeout))]
             def newFunc():
                 try:
                     res[0] = func(*args, **kwargs)
-                except Exception as e:
+                except BaseException as e:
                     res[0] = e
             t = Thread(target=newFunc)
             t.daemon = True
@@ -40,7 +41,8 @@ def execute_day(_globals, year, day, part):
     if func_name in _globals:
         start = timer()
         try:
-            result = timeout(seconds_before_timeout=EXERCISE_TIMEOUT)(_globals[func_name])(read_input(year, day))
+            result = timeout(seconds_before_timeout=EXERCISE_TIMEOUT) \
+                     (_globals[func_name])(read_input(year, day))
         except SignalCatchingError:
             result = HEAVY_EXERCISE
         end = timer()
@@ -49,5 +51,5 @@ def execute_day(_globals, year, day, part):
 def read_input(year, day):
     import os
     file_dir = os.path.dirname(os.path.realpath(__file__))
-    with open("{0}/../{1}/input/day{2}".format(file_dir,year, day),"r") as fileReader:
+    with open("{0}/../{1}/input/day{2}".format(file_dir, year, day), "r") as fileReader:
         return [line.rstrip('\n') for line in fileReader]
