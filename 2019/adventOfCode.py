e1b8cc36900fccc799774f491270b5800b07561d
Hélder Marques <helder.marques@outsystems.com>
2019-12-01 09:38:14 +0000

    Solution for day 1

--- 2019/adventOfCode.py
+++ 2019/adventOfCode.py
@@ -1,5 +1,6 @@
-
 # pylint: disable=unused-import
+# pylint: disable=import-error
+# pylint: disable=wrong-import-position
 import functools
 import math
 import multiprocessing as mp
@@ -12,21 +13,35 @@ from collections import Counter, deque
 import heapq
 from enum import Enum
 from struct import pack
-# pylint: enable=unused-import
 
-# pylint: disable=W0611
-# pylint: disable=C0413
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, FILE_DIR + "\\..\\")
-from common.utils import execute_day, read_input
-# pylint: enable=W0611
-# pylint: enable=C0413
+sys.path.insert(0, FILE_DIR + "\\..\\..\\")
+from common.utils import execute_day  # NOQA: E402
+# pylint: enable=unused-import
+# pylint: enable=import-error
+# pylint: enable=wrong-import-position
 
 """ DAY 1 """
+
+def day1_calc_fuel(mod):
+    return math.floor(mod / 3) - 2
+
 def day1_1(data):
-    return data
+    return functools.reduce(lambda a, v: a+v, [day1_calc_fuel(int(m)) for m in data])
+
+def day1_2(data):
+    total = 0
+    for mod_s in data:
+        mod = int(mod_s)
+        temp_fuel = day1_calc_fuel(mod)
+        while temp_fuel > 0:
+            total += temp_fuel
+            temp_fuel = day1_calc_fuel(temp_fuel)
+    return total
 
 """ MAIN FUNCTION """
+
 def main(specific_day):
     initial_day = 1
     end_day = 25
