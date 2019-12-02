fb26c70198b0bbfe7d93f4518a4c0010b4a7ffab
Hélder Marques <helder.marques@outsystems.com>
2019-12-02 08:00:38 +0000

    Solution for day 2 (2019)

--- 2019/adventOfCode.py
+++ 2019/adventOfCode.py
@@ -17,7 +17,7 @@ from struct import pack
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, FILE_DIR + "\\..\\")
 sys.path.insert(0, FILE_DIR + "\\..\\..\\")
-from common.utils import execute_day  # NOQA: E402
+from common.utils import execute_day, read_input  # NOQA: E402
 # pylint: enable=unused-import
 # pylint: enable=import-error
 # pylint: enable=wrong-import-position
@@ -39,6 +39,50 @@ def day1_2(data):
             total += temp_fuel
             temp_fuel = day1_calc_fuel(temp_fuel)
     return total
+
+""" DAY 2 """
+
+def day2_execute(op, pc, insts):
+    if op == 1:
+        a = insts[pc+1]
+        b = insts[pc+2]
+        c = insts[pc+3]
+        insts[c] = insts[a] + insts[b]
+        return (pc + 4, insts)
+    elif op == 2:
+        a = insts[pc+1]
+        b = insts[pc+2]
+        c = insts[pc+3]
+        insts[c] = insts[a] * insts[b]
+        return (pc + 4, insts)
+    raise NotImplementedError
+
+def day2_run_program(insts):
+    pc = 0
+    while insts[pc] != 99:
+        op = insts[pc]
+        (pc, insts) = day2_execute(op, pc, insts)
+    return insts
+
+def day2_1(data):
+    data = data[0].split(',')
+    #data = read_input(2019, 201)[0].split(',')
+    data = [int(x) for x in data]
+    data[1] = 12
+    data[2] = 2
+    return day2_run_program(data)[0]
+
+def day2_2(data):
+    data = data[0].split(',')
+    backup = [int(x) for x in data]
+    for i in range(99):
+        for j in range(99):
+            data = [int(x) for x in backup]
+            data[1] = i
+            data[2] = j
+            result = day2_run_program(data)
+            if result[0] == 19690720:
+                return 100 * result[1] + result[2]
 
 """ MAIN FUNCTION """
 
