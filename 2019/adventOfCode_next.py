281ab10e1d187788e55b3ad62aee77a659e838b2
scout719 <helder.marques1991@gmail.com>
2019-12-19 07:23:00 +0000

    On going day 19

--- 2019/adventOfCode_next.py
+++ 2019/adventOfCode_next.py
@@ -38,14 +38,103 @@ def day19_parse_input(data):
 def day19_1(data):
     #data = read_input(2019, 1901)
     data = day19_parse_input(data)
-    output = int_run_19(data, [])
-    return output
+    grid = [[0 for _ in range(50)] for _ in range(50)]
+    count =0
+    def g_i():
+        #print(pos)
+        return pos.pop(0)
+    
+    for x in range(50):
+        for y in range(50):
+            pos = [x,y]
+            
+            grid[y][x] = int_run_19(data, [], g_i)[-1]
+            count += grid[y][x]
+    
+    for r , row in enumerate(grid):
+        print()
+        for c, p in enumerate(row):
+            if p == 1:
+                print(WHITE_SQUARE, end="")
+            else:
+                print(".", end="")
+    
+    return count
+
+def checkx(data, x, y):
+    def g_i():
+        return pos.pop(0)
+    q = [(x,y, -1), (x,y,1)]
+    count = 1
+    while q:
+        x,y,d = q.pop(0)
+        pos = [x+d,y]
+        v = int_run_19(data, [], g_i)[-1]
+        if v == 1:
+           count += 1
+           q.append((x+d, y, d))
+    return count 
+def checky(data, x, y):
+    def g_i():
+        return pos.pop(0)
+    q = [(x,y, -1), (x,y,1)]
+    count = 1
+    while q:
+        x,y,d = q.pop(0)
+        pos = [x+d,y]
+        v = int_run_19(data, [], g_i)[-1]
+        if v == 1:
+           count += 1
+           q.append((x+d, y, d))
+    return count 
+
+def c(data, x,y):
+    def g_i():
+        return pos.pop(0)
+    xx,yy = x,y
+    pos = [x,y]
+    while int_run_19(data, [], g_i)[-1] == 1:
+        x +=1
+        pos =[x,y]
+    width = x - xx
+    xx = xx + (width//2)
+    pos = [xx,y]
+    while int_run_19(data, [], g_i)[-1] == 1:
+        y +=1
+        pos =[xx,y]
+    height = y - yy
+    return height, width//2
+    
 
 def day19_2(data):
+    #return
     #data = read_input(2019, 1901)
     data = day19_parse_input(data)
-    output = int_run_19(data, [])
-    return output
+    count =0
+    def g_i():
+        return pos.pop(0)
+    
+    q = [(0,0)]
+    while q:
+        x, y = q.pop(0)
+        pos=[x,y]
+        v = int_run_19(data, [], g_i)[-1]
+        if v == 1:
+            dims = c(data, x,y)
+            print(y, dims)
+            x=x
+            if y == 0:
+                y += 640
+            y+=1
+            pos = [x,y]
+            while int_run_19(data, [], g_i)[-1] == 0:
+                x +=1
+                pos =[x,y]
+            q.append((x,y))
+           
+            
+    
+    return count
 
 # IntCode logic:
 def int_run_19(insts, inputs, calculate_input=None):
@@ -53,7 +142,7 @@ def int_run_19(insts, inputs, calculate_input=None):
     pc = 0
     rel_base = 0
     outputs = []
-    while insts[pc] != 99:
+    while not outputs or insts[pc] != 99:
         op = insts[pc]
         (pc, insts, rel_base) = ic_execute(
             op, pc, insts, inputs, outputs, rel_base, calculate_input)
