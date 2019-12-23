ca69198a0cc27530f14372e9b641ba567b809421
scout719 <helder.marques1991@gmail.com>
2019-12-23 22:45:22 +0000

    Add solution day 18 part 2

--- 2019/adventOfCode_18.py
+++ 2019/adventOfCode_18.py
@@ -41,7 +41,7 @@ def all_dist2(k, r, c, grid):
             if not (rrr, ccc) in seen or seen[(rrr, ccc)] > count + 1:
                 seen[(rrr, ccc)] = count + 1
                 p = grid[rrr][ccc]
-                if p == '.' or p == "@":
+                if p == '.' or p == "@" or p == "1" or p == "2" or p == "3" or p == "4":
                     heappush(q, (rrr, ccc, count + 1))
                 elif p != "#":
                     dists[p] = (rrr, ccc, count + 1)
@@ -98,40 +98,21 @@ def day18_1(data):
     min_res = None
     seen3 = {}
     total_keys_n = len(total_keys.keys()) - 1  # remove @
+    seen = set()
     while q:
         steps, key, ks = heappop(q)
+
+        k2 = tuple([steps, key, tuple(ks)])
+        if k2 in seen:
+            continue
+        seen.add(k2)
+
         if total_keys_n == len(ks):
             return steps
 
         for other_key, s in n_pos2(key, grid, dists, ks):
-            n_ks = ks + [other_key]
-            k = tuple([other_key] + sorted(n_ks))
-            if k in seen3 and seen3[k] < steps + s:
-                continue
-            seen3[k] = steps + s
-            heappush(q, (steps + s, other_key, n_ks))
-    return min_res
-
-def test(robot, total_keys):
-    keys = []
-    for key in total_keys:
-        r, c, qq = total_keys[key]
-        if qq != robot:
-            keys.append(key)
-
-    q = [(0, str(robot), keys)]
-
-    min_res = None
-    seen3 = {}
-    total_keys_n = len(total_keys.keys()) - 1  # remove @
-    while q:
-        steps, key, ks = heappop(q)
-        if total_keys_n == len(ks):
-            return steps
-
-        for other_key, s in n_pos2(key, grid, dists, ks):
-            n_ks = ks + [other_key]
-            k = tuple([other_key] + sorted(n_ks))
+            n_ks = sorted(ks + [other_key])
+            k = tuple([other_key] + n_ks)
             if k in seen3 and seen3[k] < steps + s:
                 continue
             seen3[k] = steps + s
@@ -139,7 +120,8 @@ def test(robot, total_keys):
     return min_res
 
 def day18_2(data):
-    # data = read_input(2019, 1801)
+    if len(sys.argv) > 1:
+        data = read_input(2019, str(sys.argv[1]))
     data = day18_parse_input(data)
     total_keys = {}
     grid = []
@@ -160,7 +142,7 @@ def day18_2(data):
     for i in range(3):
         for j in range(3):
             rr, cc = start[0] + D[i], start[1] + D[j]
-            if abs(i) + abs(j) == 2:
+            if abs(D[i]) + abs(D[j]) == 2:
                 grid[rr][cc] = str(count)
                 total_keys[str(count)] = (rr, cc)
                 robots.append((rr, cc))
@@ -180,15 +162,63 @@ def day18_2(data):
     for key in total_keys:
         r, c = total_keys[key]
         if r <= robots[0][0] and c <= robots[0][1]:
-            total_keys = (r, c, 1)
+            total_keys[key] = (r, c, 1)
         if r >= robots[1][0] and c <= robots[1][1]:
-            total_keys = (r, c, 3)
+            total_keys[key] = (r, c, 3)
         if r <= robots[2][0] and c >= robots[2][1]:
-            total_keys = (r, c, 2)
+            total_keys[key] = (r, c, 2)
         if r >= robots[0][0] and c >= robots[0][1]:
-            total_keys = (r, c, 4)
+            total_keys[key] = (r, c, 4)    
+            
+    q = [(0, ["1", "2", "3", "4"], [[],[],[],[]], [])]
+
+    min_res = None
+    seen3 = {}
+    total_keys_n = len(total_keys.keys()) - 4  # remove @
+    cc = 0
+    t = 0
+    seen = set()
+    while q:
+        steps, robots, ks_r, ks = heappop(q)
+        
+        k2 = tuple([steps, tuple(robots), tuple(ks_r[0]), tuple(ks_r[1]), tuple(ks_r[2]), tuple(ks_r[3])])
+        if k2 in seen:
+            continue
+        seen.add(k2)
+
+        # if len(seen3) > 2000:
+        #     d_ks = []
+        #     for k in seen3:
+        #         if len(k) < len(ks)-4:
+        #             d_ks.append(k)
+        #     for k in d_ks:
+        #         del seen3[k]
+        c+=1
+        if c % 10000 == 0:
+            print(steps, ks, len(ks), total_keys_n, len(seen3), t)
+        if total_keys_n == len(ks):
+            return steps
+        
+        for i in range(4):
+            key = robots[i]
+            # start = timer()
+            for other_key, s in n_pos2(key, grid, dists, ks):
+                # t = (timer() - start)*1000
+                n_ks = sorted(ks + [other_key])
+                # n_ks2 = ks_r[i] + [other_key]
+                # n_ks_r = ks_r[:]
+                # n_ks_r[i] = n_ks2
+                n_robots = robots[:]
+                n_robots[i] = other_key
+                k = tuple(n_robots + n_ks)
+                if k in seen3 and seen3[k] < steps + s:
+                    continue
+                seen3[k] = steps + s
+                #q.append((steps + s, n_robots, n_ks))
+                heappush(q, (steps + s, n_robots, ks_r , n_ks))
+    return min_res
 
 """ MAIN FUNCTION """
 
 if __name__ == "__main__":
-    main(sys.argv, globals(), 2019)
+    main([""], globals(), 2019)
