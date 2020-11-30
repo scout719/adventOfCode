9a2d7e865970f21aa1f51ca7c9bcc9836b36338e
scout719 <helder.marques1991@gmail.com>
2020-11-30 12:41:15 +0000

    Setup for AoC 2020

---
+++ 2020/adventOfCode.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=import-error
+# pylint: disable=wrong-import-position
+import functools
+import math
+import os
+import sys
+import time
+from collections import defaultdict
+from heapq import *
+import copy
+
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+print(FILE_DIR)
+sys.path.insert(0, FILE_DIR + "/")
+sys.path.insert(0, FILE_DIR + "/../")
+sys.path.insert(0, FILE_DIR + "/../../")
+from common.utils import read_input, main, clear  # NOQA: E402
+# pylint: enable=import-error
+# pylint: enable=wrong-import-position
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+WHITE_SQUARE = "█"
+WHITE_CIRCLE = "•"
+BLUE_CIRCLE = f"{bcolors.OKBLUE}{bcolors.BOLD}•{bcolors.ENDC}"
+RED_SMALL_SQUARE = f"{bcolors.FAIL}{bcolors.BOLD}■{bcolors.ENDC}"
+
+""" DAY 1 """
+
+def day1_calc_fuel(mod):
+    return math.floor(mod / 3) - 2
+
+def day1_1(data):
+    return functools.reduce(lambda a, v: a + v, [day1_calc_fuel(int(m)) for m in data])
+
+def day1_2(data):
+    total = 0
+    for mod_s in data:
+        mod = int(mod_s)
+        temp_fuel = day1_calc_fuel(mod)
+        while temp_fuel > 0:
+            total += temp_fuel
+            temp_fuel = day1_calc_fuel(temp_fuel)
+    return total
+
+""" MAIN FUNCTION """
+
+if __name__ == "__main__":
+    main(sys.argv, globals(), 2020)
