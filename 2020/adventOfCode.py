8e08822cbfe9e6f88da74bf0da7bec9ef42a7478
scout719 <helder.marques1991@gmail.com>
2020-12-03 08:27:30 +0000

    Add solution day 3 2020

--- 2020/adventOfCode.py
+++ 2020/adventOfCode.py
@@ -31,6 +31,7 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 WHITE_SQUARE = "█"
 WHITE_CIRCLE = "•"
 BLUE_CIRCLE = f"{bcolors.OKBLUE}{bcolors.BOLD}•{bcolors.ENDC}"
@@ -40,23 +41,24 @@ RED_SMALL_SQUARE = f"{bcolors.FAIL}{bcolors.BOLD}■{bcolors.ENDC}"
 
 def day1_1(data):
     for i in range(len(data)):
-        for j in range(i+1, len(data)):
+        for j in range(i + 1, len(data)):
             a = int(data[i])
             b = int(data[j])
-            if a+b == 2020:
-                return a*b
+            if a + b == 2020:
+                return a * b
     return None
 
 def day1_2(data):
     for i in range(len(data)):
-        for j in range(i+1, len(data)):
-            for k in range(j+1, len(data)):
+        for j in range(i + 1, len(data)):
+            for k in range(j + 1, len(data)):
                 a = int(data[i])
                 b = int(data[j])
                 c = int(data[k])
-                if a+b+c == 2020:
-                    return a*b*c
+                if a + b + c == 2020:
+                    return a * b * c
     return None
+
 
 """ DAY 2 """
 
@@ -74,10 +76,10 @@ def day2_1(data):
         for c in password:
             if c == letter:
                 count_letter += 1
-        
+
         if min_ <= count_letter <= max_:
             count += 1
-    
+
     return count
 
 def day2_2(data):
@@ -91,10 +93,42 @@ def day2_2(data):
         min_ = int(range_.split("-")[0])
         max_ = int(range_.split("-")[1])
 
-        if (password[min_-1] == letter and password[max_-1] != letter) or \
-           (password[min_-1] != letter and password[max_-1] == letter):
+        if (password[min_ - 1] == letter and password[max_ - 1] != letter) or \
+           (password[min_ - 1] != letter and password[max_ - 1] == letter):
             count += 1
     return count
+
+
+""" DAY 3 """
+
+def day3_solve(data, dx, dy):
+    x, y = (0, 0)
+    size = len(data)
+    counter = 0
+    while y < size:
+        if data[y][x] == '#':
+            counter += 1
+        x += dx
+        x = x % len(data[0])
+        y += dy
+        y = y
+    return counter
+
+def day3_1(data):
+    #data = read_input(2020, 301)
+    return day3_solve(data, 3, 1)
+
+def day3_2(data):
+    #data = read_input(2020, 301)
+    slopes = [
+        (3, 1),
+        (1, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2)
+    ]
+    return functools.reduce(lambda a, b: a * b, [day3_solve(data, dx, dy) for (dx, dy) in slopes])
+
 
 """ MAIN FUNCTION """
 
