31918b37cb23de1df0ea2ea8bc052bfa05ca98fa
scout719 <helder.marques1991@gmail.com>
2020-12-13 23:43:03 +0000

    Turn echo off on .bat commands

--- get_input.bat
+++ get_input.bat
@@ -1,3 +1,4 @@
+@echo off
 FOR /F "TOKENS=1 eol=/ DELIMS=/ " %%A IN ('DATE/T') DO SET dd=%%A
 FOR /F "TOKENS=1,2 eol=/ DELIMS=/ " %%A IN ('DATE/T') DO SET mm=%%B
 FOR /F "TOKENS=1,2,3 eol=/ DELIMS=/ " %%A IN ('DATE/T') DO SET yyyy=%%C
