4e76eaa12460a062e6a5fe14276cdd9d25766e07
Hélder Marques <helder.marques@outsystems.com>
2021-11-29 11:43:28 +0000

    Add base code for day 1 2021

---
+++ 2021/adventOfCode_today.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=import-error
+# pylint: disable=unused-import
+# pylint: disable=wildcard-import
+# pylint: disable=wrong-import-position
+# pylint: disable=consider-using-enumerate
+import functools
+import math
+import os
+from os.path import join
+import sys
+import time
+from copy import deepcopy
+from collections import defaultdict
+from heapq import heappop, heappush
+
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, FILE_DIR + "/")
+sys.path.insert(0, FILE_DIR + "/../")
+from common.utils import read_input, main, clear  # NOQA: E402
+# pylint: enable=import-error
+# pylint: enable=wrong-import-position
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+WHITE_SQUARE = "█"
+WHITE_CIRCLE = "•"
+BLUE_CIRCLE = f"{bcolors.OKBLUE}{bcolors.BOLD}•{bcolors.ENDC}"
+RED_SMALL_SQUARE = f"{bcolors.FAIL}{bcolors.BOLD}■{bcolors.ENDC}"
+
+
+""" DAY 1 """
+
+def day1_parse(data):
+    return data
+
+def day1_1(data):
+    # data = read_input(2021, 101)
+    return day1_parse(data)
+
+def day1_2(data):
+    # data = read_input(2021, 101)
+    return day1_parse(data)
+
+
+""" MAIN FUNCTION """
+
+if __name__ == "__main__":
+    main(sys.argv, globals(), 2021)
