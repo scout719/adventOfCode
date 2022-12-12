fa09967fe9d020e640a485901bceb86ce98082b9
Hélder Marques <helder.marques@outsystems.com>
2022-12-12 09:41:43 +0000

    Tweak solution day 12 2022

--- 2022/adventOfCode_12.py
+++ 2022/adventOfCode_12.py
@@ -5,15 +5,9 @@
 # pylint: disable=unused-wildcard-import
 # pylint: disable=wrong-import-position
 # pylint: disable=consider-using-enumerate
-from typing import Callable, Dict, Iterator, Union, Optional, List, ChainMap
-import functools
-import math
 import os
-from os.path import join
 import sys
-import time
-from copy import deepcopy
-from collections import Counter, defaultdict, deque
+from collections import defaultdict
 from heapq import heappop, heappush
 
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -28,13 +22,12 @@ DAY = 12
 EXPECTED_1 = 31
 EXPECTED_2 = 29
 
-
 """ DAY 12 """
 
 def day12_parse(data):
     grid = []
     for line in data:
-        grid.append([c for c in line])
+        grid.append(list(line))
     return grid
 
 def day12_compute(data):
@@ -43,8 +36,8 @@ def day12_compute(data):
     C = len(grid[0])
     D = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     end = (0, 0)
-    start = (0,0)
-    a_s = set()
+    start = (0, 0)
+    lowest_starts = set()
     for r in range(R):
         for c in range(C):
             if grid[r][c] == "S":
@@ -52,42 +45,38 @@ def day12_compute(data):
             elif grid[r][c] == "E":
                 end = (r, c)
             elif grid[r][c] == "a":
-                a_s.add((r,c))
+                lowest_starts.add((r, c))
+    grid[end[0]][end[1]] = chr(ord('z') + 1)
+    grid[start[0]][start[1]] = chr(ord('a') - 1)
 
-    q = [(0, end)]
-    visited = set()
-    visited_m = defaultdict(int)
-    grid[end[0]][end[1]] = chr(ord('z')+1)
-    grid[start[0]][start[1]] = chr(ord('a')-1)
-    while q:
-        x = heappop(q)
-        next_s, next_p = x
-        r, c = next_p
-        if (next_p) in visited_m and 0 < visited_m[next_p] <= next_s:
+    queue = [(0, end)]
+    visited_steps = defaultdict(int)
+    while queue:
+        curr_steps, curr_pos = heappop(queue)
+        r, c = curr_pos
+        if curr_pos in visited_steps and 0 < visited_steps[curr_pos] <= curr_steps:
             continue
-        visited.add(next_p)
-        visited_m[next_p] = next_s
+        visited_steps[curr_pos] = curr_steps
         curr = grid[r][c]
         for dr, dc in D:
             rr = r + dr
             cc = c + dc
-            if 0<= rr <R and 0<=cc<C:
+            if 0 <= rr < R and 0 <= cc < C:
+                # -1 -> climbing a height of 1
+                #  0 -> stay on same level
+                # >1 -> dropping down any height
                 if ord(grid[rr][cc]) - ord(curr) >= -1:
-                    heappush(q, (next_s + 1, (rr, cc)))
-    return visited_m, start, a_s
+                    heappush(queue, (curr_steps + 1, (rr, cc)))
+    return visited_steps, start, lowest_starts
 
 def day12_1(data):
-    visited_m, start,_ = day12_compute(data)
-    return visited_m[start]
+    vistited_steps, start, _ = day12_compute(data)
+    return vistited_steps[start]
 
 def day12_2(data):
-    visited_m, _, a_s = day12_compute(data)
-    mins = set()
-    for r,c in a_s:
-        if (r,c) in visited_m:
-            mins.add(visited_m[(r,c)])
+    vistited_steps, _, lowerst_starts = day12_compute(data)
 
-    return min(mins)
+    return min(vistited_steps[(r, c)] for r, c in lowerst_starts if (r, c) in vistited_steps)
 
 
 """ MAIN FUNCTION """
