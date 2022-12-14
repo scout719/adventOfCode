a3b197cd378287a75e3e863401da0a3ec0a18754
Hélder Marques <helder.marques@outsystems.com>
2022-12-14 09:34:35 +0000

    Clean solution day 14 2022

--- 2022/adventOfCode_14.py
+++ 2022/adventOfCode_14.py
@@ -37,111 +37,62 @@ def day14_parse(data):
     for line in data:
         rock = line.split(" -> ")
         rock = [r.split(",") for r in rock]
-        rock = [(int(x),int(y)) for x,y in rock]
+        rock = [(int(x), int(y)) for x, y in rock]
         rocks.append(rock)
     return rocks
 
-def day14_1(data):
-    rocks_ = day14_parse(data)
+def day14_solve(data, part2):
+    rocks_lines = day14_parse(data)
     rocks = set()
-    for r in rocks_:
+    # Build the rock lines
+    for rock_line in rocks_lines:
         i = 0
-        while i < len(r)-1:
-            sx,sy = r[i]
-            nx,ny = r[i+1]
-            dx,dy = nx-sx, ny-sy
+        while i < len(rock_line) - 1:
+            start_x, start_y = rock_line[i]
+            end_x, end_y = rock_line[i + 1]
+            dx, dy = end_x - start_x, end_y - start_y
             if dx != 0:
-                dx = dx//abs(dx)
-                for xx in range(sx,nx+dx, dx):
-                    rocks.add((xx,sy))
+                dx = dx // abs(dx)
+                for xx in range(start_x, end_x + dx, dx):
+                    rocks.add((xx, start_y))
             elif dy != 0:
-                dy = dy//abs(dy)
-                for yy in range(sy,ny+dy, dy):
-                    rocks.add((sx,yy))
+                dy = dy // abs(dy)
+                for yy in range(start_y, end_y + dy, dy):
+                    rocks.add((start_x, yy))
             i += 1
 
-    s_x, s_y = (500,0)
-    max_y = max(y for x,y in rocks)
-    r = set()
-    D = [(0,1),(-1,1), (1,1)]
-    # print(rocks)
-    end = False
-    while not end:
-        c_x,c_y = s_x, s_y
-        while True:
-            # print(c_x, c_y)
-            # if (c_x, c_y) in rocks or (c_x,c_y) in r:
-            stopped = True
+    start_x, start_y = (500, 0)
+    max_y = max(y for x, y in rocks)
+    rested = set()
+    D = [(0, 1), (-1, 1), (1, 1)]
+    while True:
+        x, y = start_x, start_y
+        keep_falling = True
+        while keep_falling:
+            clear = False
             for dx, dy in D:
-                xx, yy = c_x + dx, c_y + dy
-                # print(xx,yy, stopped)
-                if (xx,yy) not in rocks and (xx,yy) not in r:
-                    stopped = False
-                    c_x,c_y = xx,yy
+                xx, yy = x + dx, y + dy
+                if (xx, yy) not in rocks and (xx, yy) not in rested:
+                    if part2 and yy == max_y + 2:
+                        continue
+                    clear = True
+                    x, y = xx, yy
                     break
-            if stopped:
-                # print(c_x, c_y, stopped)
-                r.add((c_x, c_y))
-                break
-            # c_y += 1
-            # print(c_x, c_y, r, max_y)
-            if c_y > max_y+1:
-                end = True
-                break
+            keep_falling = clear and y < max_y + 3
+        if y == max_y + 3:
+            assert not part2
+            return len(rested)
+        else:
+            rested.add((x, y))
+            if part2 and (x, y) == (500, 0):
+                return len(rested)
 
-    return len(r)
+
+def day14_1(data):
+    return day14_solve(data, part2=False)
 
 def day14_2(data):
-    rocks_ = day14_parse(data)
-    rocks = set()
-    for r in rocks_:
-        i = 0
-        while i < len(r)-1:
-            sx,sy = r[i]
-            nx,ny = r[i+1]
-            dx,dy = nx-sx, ny-sy
-            if dx != 0:
-                dx = dx//abs(dx)
-                for xx in range(sx,nx+dx, dx):
-                    rocks.add((xx,sy))
-            elif dy != 0:
-                dy = dy//abs(dy)
-                for yy in range(sy,ny+dy, dy):
-                    rocks.add((sx,yy))
-            i += 1
-
-    s_x, s_y = (500,0)
-    max_y = max(y for x,y in rocks)
-    r = set()
-    D = [(0,1),(-1,1), (1,1)]
-    # print(rocks)
-    end = False
-    while not end:
-        c_x,c_y = s_x, s_y
-        while True:
-            # print(c_x, c_y)
-            # if (c_x, c_y) in rocks or (c_x,c_y) in r:
-            stopped = True
-            for dx, dy in D:
-                xx, yy = c_x + dx, c_y + dy
-                # print(xx,yy, stopped)
-                if (xx,yy) not in rocks and (xx,yy) not in r and yy < max_y+2:
-                    stopped = False
-                    c_x,c_y = xx,yy
-                    break
-            if stopped:
-                if(c_x,c_y) == (500,0):
-                    return len(r)+1
-                # print(c_x, c_y, stopped)
-                r.add((c_x, c_y))
-                break
-            # c_y += 1
-            # print(c_x, c_y, r, max_y)
-            # if c_y > max_y+1:
-            #     end = True
-            #     break
-
-    return len(r)
+    return day14_solve(data, part2=True)
 
 
 """ MAIN FUNCTION """
