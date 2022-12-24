b3f8fb93b0c129bea40049304e15382f38fac8aa
Hélder Marques <helder.marques@outsystems.com>
2022-12-24 11:39:28 +0000

    Add WIP solution day 24 2022

--- 2022/adventOfCode_24.py
+++ 2022/adventOfCode_24.py
@@ -24,7 +24,7 @@ from common.utils import main, day_with_validation, RED_SMALL_SQUARE, WHITE_SQUA
 YEAR = 2022
 DAY = 24
 EXPECTED_1 = 18
-EXPECTED_2 = None
+EXPECTED_2 = 54
 
 """ DAY 24 """
 
@@ -151,6 +151,7 @@ def day24_get_bs(bs_t, t, R, C):
         bs_t[t] = new_m
         return new_m
 
+
 def day24_1(data):
     m, (s_c, e_c), (R, C) = day24_parse(data)
 
@@ -192,14 +193,15 @@ def day24_1(data):
     bs_t[0] = m
     # return day24_solve(0, 0, s_c, bs_t, R,C,e_c, s_c, {})
     q = [(0, (abs(e_c - s_c) + R - 1), (0, s_c), m)]
-    q = [((0), 0, (0, s_c))]
+    q = [((0,0), 0, (0, s_c))]
     # q = deque(q)
     seen = set()
-    D = [(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)]
+    D = [(1, 0), (0, 1), (0,-1), (-1, 0), (0, 0)]
     print(s_c, e_c)
+    print(sum(len(v) for v in m.values()))
     best = 1e9
     while q:
-        _, t, (r, c) = heappop(q)
+        (_,w), t, (r, c) = heappop(q)
         # print(t)
         # _, t, (r,c) = q.popleft()
         # print(cost,r,c,curr_m[(r,c)])
@@ -212,10 +214,12 @@ def day24_1(data):
 
         curr_m = day24_get_bs(bs_t, t, R, C)
         # day24_print(r, c, curr_m, R, C)
-        # time.sleep(2)
+        # time.sleep(.5)
         l = []
         for (rr, cc), bs in curr_m.items():
             for (dr, dc) in bs:
+                if (rr,cc) < (r,c):
+                    continue
                 l.append((rr, cc, dr, dc))
 
         k = (r, c, tuple(sorted(l)))
@@ -225,6 +229,7 @@ def day24_1(data):
         seen.add(k)
         moved = False
         for dr, dc in D:
+            # if moved: continue
             rr, cc = r + dr, c + dc
             new_m = day24_get_bs(bs_t, t + 1, R, C)
             if len(new_m[(rr, cc)]) > 0:
@@ -242,12 +247,14 @@ def day24_1(data):
             # if dr==dc==0:
             #     continue
             moved = True
-            heappush(q, (t + 1 + (abs(e_c - cc) +
-                     abs(R - 1 - rr)), t + 1, (rr, cc)))
+            w2 = 1 if dr==dc==0 else 0
+            heappush(q, (((abs(e_c - cc) +
+                     abs(R - 1 - rr))+t+1,0), t + 1, (rr, cc)))
             # heappush(q, ((0), t+1, (r+dr, c+dc), deepcopy(new_m)))
             # q.append(((0), t+1, (rr,cc)))
         # if not moved:
-        #     q.append(((0), t+1, (r,c)))
+        #     heappush(q, (t + 1 + (abs(e_c - c) +
+        #              abs(R - 1 - r)), t + 1, (r, c)))
 
     # 355
     # 354
@@ -255,8 +262,227 @@ def day24_1(data):
     return best
 
 def day24_2(data):
-    m = day24_parse(data)
-    return ""
+    m, (s_c, e_c), (R, C) = day24_parse(data)
+
+    # seen_m = set()
+    # t = 0
+    # curr_m = m
+    # while True:
+    #     t+=1
+    #     l = deque([])
+    #     for (rr,cc),bs in curr_m.items():
+    #         for (dr,dc) in bs:
+    #             l.append((rr,cc,dr,dc))
+    #     k = (tuple(sorted(l)))
+    #     if k in seen_m:
+    #         assert False, t
+    #         continue
+
+    #     new_m = defaultdict(list)
+    #     for (rr,cc),bs in curr_m.items():
+    #         for dr,dc in bs:
+    #             rrr,ccc = rr+dr, cc+dc
+    #             if rrr == R-1:
+    #                 assert dr == 1
+    #                 rrr = 1
+    #             elif rrr == 0:
+    #                 assert dr == -1
+    #                 rrr = R-2
+    #             if ccc == C-1:
+    #                 assert dc == 1
+    #                 ccc = 1
+    #             elif ccc == 0:
+    #                 assert dc == -1
+    #                 ccc = C-2
+
+    #             new_m[(rrr,ccc)].append((dr,dc))
+    #     curr_m = new_m
+
+    bs_t = {}
+    bs_t[0] = m
+    # return day24_solve(0, 0, s_c, bs_t, R,C,e_c, s_c, {})
+    q = [(0, (abs(e_c - s_c) + R - 1), (0, s_c), m)]
+    q = [((0,0), 0, (0, s_c))]
+    # q = deque(q)
+    seen = set()
+    D = [(1, 0), (0, 1), (0,-1), (-1, 0), (0, 0)]
+    print(s_c, e_c)
+    print(sum(len(v) for v in m.values()))
+    best = 1e9
+    while q:
+        (_,w), t, (r, c) = heappop(q)
+        # print(t)
+        # _, t, (r,c) = q.popleft()
+        # print(cost,r,c,curr_m[(r,c)])
+        # if cost == 6:
+        #     assert False
+        # if len(m[(r,c)]) != 0:
+        #     continue
+        if (r, c) == (R - 1, e_c):
+            best = t
+            break
+            # return t
+
+        curr_m = day24_get_bs(bs_t, t, R, C)
+        # day24_print(r, c, curr_m, R, C)
+        # time.sleep(.5)
+        l = []
+        for (rr, cc), bs in curr_m.items():
+            for (dr, dc) in bs:
+                if (rr,cc) < (r,c):
+                    continue
+                l.append((rr, cc, dr, dc))
+
+        k = (r, c, tuple(sorted(l)))
+        if k in seen:
+            # print("x")
+            continue
+        seen.add(k)
+        moved = False
+        for dr, dc in D:
+            # if moved: continue
+            rr, cc = r + dr, c + dc
+            new_m = day24_get_bs(bs_t, t + 1, R, C)
+            if len(new_m[(rr, cc)]) > 0:
+                continue
+            if not (0 <= rr < R and 0 <= cc < C):
+                continue
+            if rr == 0:
+                continue
+            if rr == R - 1 and cc != e_c:
+                continue
+            if cc == 0:
+                continue
+            if cc == C - 1:
+                continue
+            # if dr==dc==0:
+            #     continue
+            moved = True
+            w2 = 1 if dr==dc==0 else 0
+            heappush(q, (((abs(e_c - cc) +
+                     abs(R - 1 - rr))+t+1,0), t + 1, (rr, cc)))
+            # heappush(q, ((0), t+1, (r+dr, c+dc), deepcopy(new_m)))
+            # q.append(((0), t+1, (rr,cc)))
+        # if not moved:
+        #     heappush(q, (t + 1 + (abs(e_c - c) +
+        #              abs(R - 1 - r)), t + 1, (r, c)))
+
+    q = [((0,0), best, (R-1, e_c))]
+    # q = deque(q)
+    seen = set()
+    D = [(1, 0), (0, 1), (0,-1), (-1, 0), (0, 0)]
+    # best = 1e9
+    while q:
+        (_,w), t, (r, c) = heappop(q)
+        # print(t)
+        # _, t, (r,c) = q.popleft()
+        # print(cost,r,c,curr_m[(r,c)])
+        # if cost == 6:
+        #     assert False
+        # if len(m[(r,c)]) != 0:
+        #     continue
+        if (r, c) == (0, s_c):
+            best += t
+            break
+
+        curr_m = day24_get_bs(bs_t, t, R, C)
+        # day24_print(r, c, curr_m, R, C)
+        # time.sleep(.5)
+        l = []
+        for (rr, cc), bs in curr_m.items():
+            for (dr, dc) in bs:
+                if (rr,cc) > (r,c):
+                    continue
+                l.append((rr, cc, dr, dc))
+
+        k = (r, c, tuple(sorted(l)))
+        if k in seen:
+            # print("x")
+            continue
+        seen.add(k)
+        moved = False
+        for dr, dc in D:
+            # if moved: continue
+            rr, cc = r + dr, c + dc
+            new_m = day24_get_bs(bs_t, t + 1, R, C)
+            if len(new_m[(rr, cc)]) > 0:
+                continue
+            if not (0 <= rr < R and 0 <= cc < C):
+                continue
+            if rr == 0:
+                continue
+            if rr == R - 1 and cc != s_c:
+                continue
+            if cc == 0:
+                continue
+            if cc == C - 1:
+                continue
+            # if dr==dc==0:
+            #     continue
+            moved = True
+            w2 = 1 if dr==dc==0 else 0
+            heappush(q, (((abs(e_c - cc) +
+                     abs(R - 1 - rr))+t+1,0), t + 1, (rr, cc)))
+    q = [((0,0), best, (0, s_c))]
+    # q = deque(q)
+    seen = set()
+    D = [(1, 0), (0, 1), (0,-1), (-1, 0), (0, 0)]
+    # best = 1e9
+    while q:
+        (_,w), t, (r, c) = heappop(q)
+        # print(t)
+        # _, t, (r,c) = q.popleft()
+        # print(cost,r,c,curr_m[(r,c)])
+        # if cost == 6:
+        #     assert False
+        # if len(m[(r,c)]) != 0:
+        #     continue
+        if (r, c) == (R-1, e_c):
+            best += t
+            break
+
+        curr_m = day24_get_bs(bs_t, t, R, C)
+        # day24_print(r, c, curr_m, R, C)
+        # time.sleep(.5)
+        l = []
+        for (rr, cc), bs in curr_m.items():
+            for (dr, dc) in bs:
+                if (rr,cc) < (r,c):
+                    continue
+                l.append((rr, cc, dr, dc))
+
+        k = (r, c, tuple(sorted(l)))
+        if k in seen:
+            # print("x")
+            continue
+        seen.add(k)
+        moved = False
+        for dr, dc in D:
+            # if moved: continue
+            rr, cc = r + dr, c + dc
+            new_m = day24_get_bs(bs_t, t + 1, R, C)
+            if len(new_m[(rr, cc)]) > 0:
+                continue
+            if not (0 <= rr < R and 0 <= cc < C):
+                continue
+            if rr == 0:
+                continue
+            if rr == R - 1 and cc != e_c:
+                continue
+            if cc == 0:
+                continue
+            if cc == C - 1:
+                continue
+            # if dr==dc==0:
+            #     continue
+            moved = True
+            w2 = 1 if dr==dc==0 else 0
+            heappush(q, (((abs(e_c - cc) +
+                     abs(R - 1 - rr))+t+1,0), t + 1, (rr, cc)))
+    # 355
+    # 354
+    # 356
+    return best
 
 
 """ MAIN FUNCTION """
