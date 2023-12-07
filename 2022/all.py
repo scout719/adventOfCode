2f1060ceb609f762639aceea8c5f9371011c143a
scout719 <helder.marques1991@gmail.com>
2023-12-07 00:50:34 +0000

    Add *all* file for 2022, 2023

---
+++ 2022/all.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=wrong-import-position
+import os
+import sys
+
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, FILE_DIR + "/")
+sys.path.insert(0, FILE_DIR + "/../")
+from common.utils import main  # NOQA: E402
+# pylint: enable=wrong-import-position
+
+""" MAIN FUNCTION """
+
+if __name__ == "__main__":
+    import importlib
+    import inspect
+    for i in range(1, 26):
+        try:
+            m = importlib.import_module(f"adventOfCode_{i:02}")
+            for k, v in inspect.getmembers(m):
+                if k in [f"day{i}_1", f"day{i}_2"]:
+                    globals()[k] = v
+        except ModuleNotFoundError:
+            pass
+    main(sys.argv, globals(), 2022)
