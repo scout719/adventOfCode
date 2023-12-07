e2db5dd6f53c50700488c1a6b895b8f4b3b1aa31
scout719 <helder.marques1991@gmail.com>
2023-12-07 15:53:20 +0000

    Cleanup solution day 7 2023

--- 2023/adventOfCode_07.py
+++ 2023/adventOfCode_07.py
@@ -3,17 +3,13 @@
 import os
 import sys
 from collections import defaultdict
-from functools import cmp_to_key
-import math
-from typing import List, Tuple, Set, Dict
+from typing import List
 
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, FILE_DIR + "/")
 sys.path.insert(0, FILE_DIR + "/../")
 from common.utils import day_with_validation  # NOQA: E402
 from common.utils import main  # NOQA: E402
-
-Card = Tuple[int, Set[int], Set[int]]
 
 YEAR = 2023
 DAY = 7
@@ -26,117 +22,73 @@ EXPECTED_2 = 5905
 def day7_parse(data: List[str]):
     return [(line.split()[0], int(line.split()[1])) for line in data]
 
-def day7_strength(card):
-    M: List[str] = list(reversed(["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]))
-    assert card in M, card
-    return M.index(card)
+def day7_strength(card, part2: bool):
+    cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+    if part2:
+        # J is the least valuable
+        cards.remove("J")
+        cards.append("J")
+    S: List[str] = list(reversed(cards))
+    assert card in S, card
+    return S.index(card)
 
-def day7_strength2(card):
-    M: List[str] = list(reversed(["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]))
-    assert card in M, card
-    return M.index(card)
-
-def day7_type(hand):
+def day7_key(hand, part2):
     counts = defaultdict(int)
     for c in hand:
-        counts[c]+=1
+        counts[c] += 1
     s_counts = []
-    for k,v in counts.items():
-        s_counts.append((v,k))
-    s_counts = sorted(s_counts, reverse=True)
-
-    if len(s_counts) == 1:
-        return 6
-    if s_counts[0][0] == 4:
-        return 5
-    if s_counts[0][0] == 3 and s_counts[1][0] == 2:
-        return 4
-    if s_counts[0][0] == 3 and len(s_counts) == 3:
-        return 3
-    if s_counts[0][0] == 2 and s_counts[1][0] == 2:
-        return 2
-    if s_counts[0][0] == 2 and len(s_counts) == 4:
-        return 1
-    assert len(s_counts) == 5
-    return 0
-
-def day7_type2(hand):
-    counts = defaultdict(int)
-    for c in hand:
-        counts[c]+=1
-    s_counts = []
-    for k,v in counts.items():
-        if k == "J":
+    for k, v in counts.items():
+        if k == "J" and part2:
+            # skip J's
             continue
-        s_counts.append([v,k])
+        s_counts.append([v, k])
 
     s_counts = sorted(s_counts, reverse=True)
-    J_c = counts["J"]
-    if len(s_counts) == 0:
-        # only J
-        s_counts = [[5,"J"]]
-    s_counts[0][0] += J_c
+
+    if part2:
+        # add J count to highest
+        J_c = counts["J"]
+        if len(s_counts) == 0:
+            # only J
+            s_counts = [[5, "J"]]
+        s_counts[0][0] += J_c
 
     if len(s_counts) == 1:
-        return 6
-    if s_counts[0][0] == 4:
-        return 5
-    if s_counts[0][0] == 3 and s_counts[1][0] == 2:
-        return 4
-    if s_counts[0][0] == 3 and len(s_counts) == 3:
-        return 3
-    if s_counts[0][0] == 2 and s_counts[1][0] == 2:
-        return 2
-    if s_counts[0][0] == 2 and len(s_counts) == 4:
-        return 1
-    assert len(s_counts) == 5
-    return 0
+        hand_type = 6
+    elif s_counts[0][0] == 4:
+        hand_type = 5
+    elif s_counts[0][0] == 3 and s_counts[1][0] == 2:
+        hand_type = 4
+    elif s_counts[0][0] == 3 and len(s_counts) == 3:
+        hand_type = 3
+    elif s_counts[0][0] == 2 and s_counts[1][0] == 2:
+        hand_type = 2
+    elif s_counts[0][0] == 2 and len(s_counts) == 4:
+        hand_type = 1
+    else:
+        assert len(s_counts) == 5
+        hand_type = 0
 
-def day7_sort(a: Tuple[str,int],b: Tuple[str,int]) -> int:
-    type_a = day7_type(a[0])
-    type_b = day7_type(b[0])
-    if type_a != type_b:
-        return type_a-type_b
-    for i in range(len(a[0])):
-        s_a = day7_strength(a[0][i])
-        s_b = day7_strength(b[0][i])
-        if s_a != s_b:
-            return s_a-s_b
-    return 0
+    strengths = [day7_strength(card, part2) for card in hand]
+    return hand_type, strengths
 
-def day7_sort2(a: Tuple[str,int],b: Tuple[str,int]) -> int:
-    type_a = day7_type2(a[0])
-    type_b = day7_type2(b[0])
-    if type_a != type_b:
-        return type_a-type_b
-    for i in range(len(a[0])):
-        s_a = day7_strength2(a[0][i])
-        s_b = day7_strength2(b[0][i])
-        if s_a != s_b:
-            return s_a-s_b
-    return 0
+def day7_solve(data, part2):
+    hands = day7_parse(data)
+    hands = sorted(hands, key=lambda k: day7_key(k[0], part2), reverse=False)
+    hands_only = [hand[0] for hand in hands]
+    assert len(hands_only) == len(set(hands_only))
+    ans = 0
+    for i, hand in enumerate(hands):
+        ans += (i + 1) * hand[1]
+    return ans
 
 def day7_1(data: List[str]):
-    hands = day7_parse(data)
-    hands = sorted(hands, key=cmp_to_key(day7_sort), reverse=False)
-    hands_only = [hand[0] for hand in hands]
-    assert len(hands_only) == len(set(hands_only))
-    ans = 0
-    for i, hand in enumerate(hands):
-        ans += (i+1)*hand[1]
     # 247898031 low
     # 248843302 hig
-    return ans
+    return day7_solve(data, False)
 
 def day7_2(data: List[str]):
-    hands = day7_parse(data)
-    hands = sorted(hands, key=cmp_to_key(day7_sort2), reverse=False)
-    hands_only = [hand[0] for hand in hands]
-    assert len(hands_only) == len(set(hands_only))
-    ans = 0
-    for i, hand in enumerate(hands):
-        ans += (i+1)*hand[1]
-    return ans
+    return day7_solve(data, True)
 
 
 """ MAIN FUNCTION """
