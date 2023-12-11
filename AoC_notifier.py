ceb1b2f9fa8ccc960ebe9da591b28cc807caa99c
scout719 <helder.marques1991@gmail.com>
2023-12-11 09:01:20 +0000

    Add AoC leaderboard notifier

---
+++ AoC_notifier.py
@@ -0,0 +1,53 @@
+import json
+import os
+import time
+from datetime import datetime, timedelta
+import traceback
+import requests
+from win11toast import notify
+
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+def process_board(id_, last_check, year, session):
+    res = requests.get(
+        f"https://adventofcode.com/{year}/leaderboard/private/view/{id_}.json",
+        cookies={"session": session}, timeout=5).text
+    obj = json.loads(res)
+    l = []
+    for u_ in obj["members"]:
+        u = obj["members"][u_]
+        for d_ in u['completion_day_level']:
+            d = u['completion_day_level'][d_]
+            for s_ in d:
+                s = d[s_]
+                ts = datetime.fromtimestamp(int(s['get_star_ts']))
+                # delta = datetime.now() - ts
+                # if delta.total_seconds() <= 600000:
+                msg = f"{ts.time()} [Day {d_} star {s_}] {u['name']}"
+                l.append((ts, msg))
+
+    for ts, msg in sorted(l):
+        if not last_check or ts > last_check:
+            print(msg)
+            notify(f"AoC {year}", msg, icon=f"{FILE_DIR}\\AoC.png")
+
+def main():
+    year = datetime.now().year
+    sleep = 20
+    # toaster = ToastNotifier()
+    session = open("session", encoding="ascii").readline()
+    last_check = datetime.now() - timedelta(hours=6)
+    boards = [321349, 194943]
+    while True:
+        try:
+            for id_ in boards:
+                process_board(id_, last_check, year, session)
+            last_check = datetime.now()
+        except Exception:  # pylint: disable=broad-exception-caught
+            traceback.print_exc()
+        # print("sleeping", last_check)
+        time.sleep(sleep)
+
+
+if __name__ == "__main__":
+    main()
