b0e36445e9e1a71ed01c0f20bcd95912c051036a
scout719 <helder.marques1991@gmail.com>
2023-12-23 12:09:56 +0000

    Tweak solution day 23 2023

--- 2023/adventOfCode_23.py
+++ 2023/adventOfCode_23.py
@@ -15,7 +15,7 @@ from common.utils import main  # NOQA: E402
 
 YEAR = 2023
 DAY = 23
-EXPECTED_1 = None  # 94
+EXPECTED_1 = 94
 EXPECTED_2 = 154
 
 """ DAY 23 """
@@ -36,7 +36,7 @@ def day23_show(grid):
             if ch != "#" and r != 0 and r != R - 1:
                 ct = 0
                 D: list[tuple[int, int]] = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-                for i, (dr, dc) in enumerate(D):
+                for _, (dr, dc) in enumerate(D):
                     rr, cc = r + dr, c + dc
                     if grid[rr][cc] != "#":
                         ct += 1
@@ -47,20 +47,21 @@ def day23_show(grid):
     print((junc, len(junc)))
 
 def day23_path(r, c, rr, cc, juncs, grid):
-    q = [(r, c, [])]
+    q = [(r, c, 0)]
     D: list[tuple[int, int]] = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     R = len(grid)
     C = len(grid[0])
     paths = set()
+    seen = set()
     while q:
-        rrr, ccc, path = q.pop()
-        if (rrr, ccc) in path:
+        rrr, ccc, d = q.pop()
+        if (rrr, ccc) in seen:
             continue
-        path.append((rrr, ccc))
+        seen.add((rrr, ccc))
 
         if (rrr, ccc) == (rr, cc):
-            paths.add(len(path)-1)
-            continue
+            # originally was looking for the longest path
+            return d
 
         if (rrr, ccc) in juncs and (rrr, ccc) != (r, c):
             continue
@@ -68,14 +69,59 @@ def day23_path(r, c, rr, cc, juncs, grid):
         for dr, dc in D:
             rrrr, cccc = rrr + dr, ccc + dc
             if 0 <= rrrr < R and 0 <= cccc < C and grid[rrrr][cccc] != "#":
-                q.append((rrrr, cccc, deepcopy(path)))
+                q.append((rrrr, cccc, d + 1))
 
     return max(paths) if paths else None
 
 def day23_solve(x, part2):
-    if not part2:
-        return None
     grid: list[list[str]] = x
+    R = len(grid)
+    C = len(grid[0])
+    s_c = grid[0].index(".")
+    e_c = grid[-1].index(".")
+
+    D: list[tuple[int, int]] = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    paths = []
+    q: list[tuple[int, int, int, list[tuple[int, int]]]] = [
+        (0, s_c, 0, [])]
+    while q:
+        r, c, l, path = q.pop()
+        if (r, c) == (R - 1, e_c):
+            paths.append(len(path))
+            continue
+        for _, (dr, dc) in enumerate(D):
+            rr, cc = r + dr, c + dc
+            if (rr, cc) in path:
+                continue
+            if grid[rr][cc] == ".":
+                q.append(
+                        (rr, cc,
+                         l + 1, path + [(rr, cc)]))
+                continue
+            ch = grid[rr][cc]
+            if ch == ">" and dc == 1:
+                q.append(
+                    (rr, cc + 1, l + 2, path + [(rr, cc), (rr, cc + 1)]))
+            elif ch == "<" and dc == -1:
+                q.append(
+                    (rr, cc - 1, l + 2, path + [(rr, cc), (rr, cc - 1)]))
+            elif ch == "^" and dr == -1:
+                q.append(
+                    (rr - 1, cc, l + 2, path + [(rr, cc), (rr + 1, cc)]))
+            elif ch == "v" and dr == 1:
+                q.append(
+                    (rr + 1, cc, l + 2, path + [(rr, cc), (rr - 1, cc)]))
+            else:
+                pass
+    return max(paths)
+
+def day23_1(data):
+    x = day23_parse(data)
+    return day23_solve(x, False)
+
+def day23_2(data: list[str]):
+    x = day23_parse(data)
+    grid: list[str] = x
     R = len(grid)
     C = len(grid[0])
     s_c = grid[0].index(".")
@@ -110,164 +156,28 @@ def day23_solve(x, part2):
                 Dist[(right, left)] = d
                 E[left].add(right)
                 E[right].add(left)
-    # print(Dist)
-    q = [(0, 0, s_c, [])]
+
+    # DFS would not require a seen set for each elem since it explores a path at a time
+    # so we could just use a single Bit array and, each time we finish going down
+    # a path, unmark it
+    q = [(0, 0, s_c, set())]
     paths = set()
     while q:
         l, r, c, path = q.pop()
         if (r, c) in path:
             continue
-        path.append((r, c))
+        path.add((r, c))
 
         if (r, c) == (R - 1, e_c):
             paths.add(l)
-            # print(max(paths))
             continue
         for rr, cc in E[(r, c)]:
             if (rr, cc) in path:
                 continue
             d = Dist[((r, c), (rr, cc))]
-            q.append((l + d, rr, cc, deepcopy(path)))
+            q.append((l + d, rr, cc, set() | path))
 
     return max(paths)
-    total_pos = sum(1 if grid[r][c] in [".", "<", ">", "^", "v"]
-                    else 0 for r in range(R) for c in range(C))
-
-    D: list[tuple[int, int]] = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    paths = []
-    q: list[tuple[int, int, int, int, int, list[tuple[int, int]]]] = [
-        (0, 0, 0, s_c, 0, [])]
-    seen = {}
-    while q:
-        l2, dir_, r, c, l, path = heappop(q)
-        k = r, c, dir_
-        # if k in seen and seen[k] > len(path):
-        #     continue
-        # seen[k] = len(path)
-        if (r, c) == (R - 1, e_c):
-            paths.append(len(path))
-            # return len(path)
-            print(len(path))
-            continue
-        for i, (dr, dc) in enumerate(D):
-            rr, cc = r + dr, c + dc
-            if (rr, cc) in path:
-                continue
-            n_l2 = l
-            if grid[rr][cc] == ".":
-                heappush(q, (l2 + 1, i, rr, cc,
-                         l + 1, path + [(rr, cc)]))
-                # q.append((rr, cc, l + 1, path + [(rr, cc)]))
-                continue
-            ch = grid[rr][cc]
-            if part2:
-                if ch in [">", "<", "^", "v"]:
-                    heappush(q, (l2 + 1, i, rr, cc,
-                             l + 1, path + [(rr, cc)]))
-                    # q.append((rr, cc, l + 1, path + [(rr, cc)]))
-                    continue
-            if ch == ">" and dc == 1:
-
-                heappush(q,
-                         (l2 + 2, i, rr, cc + 1, l + 2, path + [(rr, cc), (rr, cc + 1)]))
-                # q.append(
-                # heappush(q,
-                #          (l-2, rr, cc + 1, l + 2, path + [(rr, cc), (rr, cc + 1)]))
-            elif ch == "<" and dc == -1:
-                # q.append(
-                heappush(q,
-                         (l2 + 2, i, rr, cc - 1, l + 2, path + [(rr, cc), (rr, cc - 1)]))
-            elif ch == "^" and dr == -1:
-                # q.append(
-                heappush(q,
-                         (l2 + 2, i, rr - 1, cc, l + 2, path + [(rr, cc), (rr + 1, cc)]))
-            elif ch == "v" and dr == 1:
-                # q.append(
-                heappush(q,
-                         (l2 + 2, i, rr + 1, cc, l + 2, path + [(rr, cc), (rr - 1, cc)]))
-            else:
-                # assert ch == "#", ch
-                pass
-
-    return max(paths)
-
-# def day23_solve(x, part2):
-#     grid: list[list[str]] = x
-#     R = len(grid)
-#     C = len(grid[0])
-#     s_c = grid[0].index(".")
-#     e_c = grid[-1].index(".")
-
-#     total_pos = sum(1 if grid[r][c] in [".", "<", ">", "^", "v"]
-#                     else 0 for r in range(R) for c in range(C))
-
-#     D: list[tuple[int, int]] = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-#     paths = []
-#     q: list[tuple[int, int, int, int, int, list[tuple[int, int]]]] = [
-#         (0 + R + C, 0, 0, s_c, 0, [])]
-#     seen = {}
-#     while q:
-#         l2, dir_, r, c, l, path = heappop(q)
-#         k = r, c, dir_
-#         # if k in seen and seen[k] > len(path):
-#         #     continue
-#         # seen[k] = len(path)
-#         if (r, c) == (R - 1, e_c):
-#             paths.append(len(path))
-#             # return len(path)
-#             print(len(path))
-#             continue
-#         for i, (dr, dc) in enumerate(D):
-#             rr, cc = r + dr, c + dc
-#             if (rr, cc) in path:
-#                 continue
-#             if grid[rr][cc] == ".":
-#                 heappush(q, (l2 - 1 + rr + cc, i, rr, cc,
-#                          l + 1, path + [(rr, cc)]))
-#                 # q.append((rr, cc, l + 1, path + [(rr, cc)]))
-#                 continue
-#             ch = grid[rr][cc]
-#             if part2:
-#                 if ch in [">", "<", "^", "v"]:
-#                     heappush(q, (l2 - 1 + rr + cc, i, rr, cc,
-#                              l + 1, path + [(rr, cc)]))
-#                     # q.append((rr, cc, l + 1, path + [(rr, cc)]))
-#                     continue
-#             if ch == ">" and dc == 1:
-
-#                 heappush(q,
-#                          (l2 - 2+rr+cc, i, rr, cc + 1, l + 2, path + [(rr, cc), (rr, cc + 1)]))
-#                 # q.append(
-#                 # heappush(q,
-#                 #          (l-2, rr, cc + 1, l + 2, path + [(rr, cc), (rr, cc + 1)]))
-#             elif ch == "<" and dc == -1:
-#                 # q.append(
-#                 heappush(q,
-#                          (l2 - 2+rr+cc, i, rr, cc - 1, l + 2, path + [(rr, cc), (rr, cc - 1)]))
-#             elif ch == "^" and dr == -1:
-#                 # q.append(
-#                 heappush(q,
-#                          (l2 - 2+rr+cc, i, rr - 1, cc, l + 2, path + [(rr, cc), (rr + 1, cc)]))
-#             elif ch == "v" and dr == 1:
-#                 # q.append(
-#                 heappush(q,
-#                          (l2 - 2+rr+cc, i, rr + 1, cc, l + 2, path + [(rr, cc), (rr - 1, cc)]))
-#             else:
-#                 # assert ch == "#", ch
-#                 pass
-
-#     return max(paths)
-
-
-def day23_1(data):
-    x = day23_parse(data)
-    return day23_solve(x, False)
-
-def day23_2(data: list[str]):
-    x = day23_parse(data)
-    # 5694 low
-    # 6500 not right
-    return day23_solve(x, True)
 
 
 """ MAIN FUNCTION """
