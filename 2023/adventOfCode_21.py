7d412efe3d392d236f51f8ffbced0118bcfbf707
scout719 <helder.marques1991@gmail.com>
2023-12-23 23:21:26 +0000

    Clean solution day 21 2023

--- 2023/adventOfCode_21.py
+++ 2023/adventOfCode_21.py
@@ -14,7 +14,7 @@ from common.utils import main  # NOQA: E402
 
 YEAR = 2023
 DAY = 21
-EXPECTED_1 = None
+EXPECTED_1 = 16
 EXPECTED_2 = None
 
 """ DAY 21 """
@@ -22,8 +22,7 @@ EXPECTED_2 = None
 def day21_parse(data: list[str]):
     return data
 
-def day21_solve(x, part2):
-    grid = x
+def day21_info(grid):
     R = len(grid)
     C = len(grid[0])
     rocks = set()
@@ -36,81 +35,63 @@ def day21_solve(x, part2):
                 start = (r, c)
             else:
                 assert grid[r][c] == "."
+    return start, rocks
 
-    st = 6 if R == 11 else 64
+def day21_solve(x, _):
+    grid = x
+    R = len(grid)
+    C = len(grid[0])
+    start, rocks = day21_info(grid)
+
+    steps = 6 if R == 11 else 64
     q = set([start])
-    seen = set()
     D = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-    for t in range(st):
-        # print(q)
+    for _ in range(steps):
         n_q = set()
         for r, c in q:
-            # seen.add((r, c))
             for dr, dc in D:
                 rr, cc = r + dr, c + dc
-                if 0 <= rr < R and 0 <= cc < C and (rr, cc) not in seen and (rr, cc) not in rocks:
+                if 0 <= rr < R and 0 <= cc < C and (rr, cc) not in rocks:
                     n_q.add((rr, cc))
         q = n_q
-    # print(q)
     return len(q)
-
-    return None
 
 def day21_1(data):
     x = day21_parse(data)
     return day21_solve(x, False)
 
-
-def day20_dp(r, c, st, R, C, rocks, mem):
-    if st == 0:
-        return set([(r, c)])
-
-    else:
-        k = (r, c, st)
-        if k in mem:
-            return mem[k]
-
-        D = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-        ans = set()
-        for dr, dc in D:
-            rr, cc = (r + dr) % R, (c + dc) % C
-
-            if (rr, cc) not in rocks:
-                ans = ans.union(day20_dp(rr, cc, st - 1, R, C, rocks, mem))
-
-        # print(rr,cc,st, ans)
-        mem[k] = ans
-        return ans
-
 def day21_reachable(R, C, rocks, r, c, max_l):
-    q2: deque[tuple[tuple[int, int], int]] = deque([((r, c), 0)])
+    q: deque[tuple[tuple[int, int], int]] = deque([((r, c), 0)])
     seen = set()
     reacheable_odd = set()
     reacheable_even = set()
-    while q2:
-        (r, c), l = q2.popleft()
+    while q:
+        (r, c), steps = q.popleft()
         k = (r, c)
         if k in seen:
             continue
         seen.add(k)
-        if l % 2 == 1:
-            reacheable_odd.add((k, l))
+        if steps % 2 == 1:
+            reacheable_odd.add((k, steps))
         else:
-            reacheable_even.add((k, l))
+            reacheable_even.add((k, steps))
         D = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         for dr, dc in D:
             rr, cc = (r + dr), (c + dc)
             if (rr, cc) not in rocks and 0 <= rr < R and 0 <= cc < C:
-                q2.append(((rr, cc), l + 1))
-    return [x for x, l in reacheable_odd if l <= max_l], [x for x, l in reacheable_even if l <= max_l]
+                q.append(((rr, cc), steps + 1))
+    return ([x for x, steps in reacheable_odd if steps <= max_l],
+            [x for x, steps in reacheable_even if steps <= max_l])
 
-def day21_total(R, C, rocks, st, mid_r, mid_c, corner_r, corner_c):
+def day21_quadrant(R, C, rocks, st, mid_r, mid_c, corner_r, corner_c):
+    # https://docs.google.com/spreadsheets/d/1St7OyhUKuvzdPOLTK7qejzADFwP-LLFuJOm5MUsJBQ4/edit?usp=sharing
+    # Total of a quadrant of the diamond
+
     mid_odd, mid_even = day21_reachable(R, C, rocks, mid_r, mid_c, R + C)
     corner_odd, corner_even = day21_reachable(
         R, C, rocks, corner_r, corner_c, R + C)
 
-    n_full = (st // R) - 1
-    full_line = n_full
+    full_line = (st // R) - 1
     full_line_even = full_line // 2
     full_line_odd = full_line - full_line_even
     assert full_line_even <= full_line_odd
@@ -120,17 +101,13 @@ def day21_total(R, C, rocks, st, mid_r, mid_c, corner_r, corner_c):
     extreme_half_full = len(
         extreme_odd) if extreme_half_full_is_odd else len(extreme_even)
 
-    total_full = (n_full * (n_full+1)) // 2
-    full_diags = total_full - n_full
+    total_full = (full_line * (full_line + 1)) // 2
+    full_diags = total_full - full_line
     y = max(0, (full_line - 2) if (full_line - 1) %
             2 == 0 else (full_line - 1))
-    y = (y//2)+1
+    y = (y // 2) + 1
     full_diag_odd = y * y
-    z = max(0, (full_line - 1) if (full_line - 1) %
-            2 == 0 else (full_line - 2))
-    full_diag_even = full_diags - full_diag_odd  # (z // 2) * ((z // 2) + 1)
-    assert full_diag_even == full_diag_odd == 0 or full_diag_even == (
-        full_diags - full_diag_odd)
+    full_diag_even = full_diags - full_diag_odd
 
     half_full_partial = full_line
     half_empty_partial = full_line + 1
@@ -138,9 +115,9 @@ def day21_total(R, C, rocks, st, mid_r, mid_c, corner_r, corner_c):
     half_empty_partial_is_odd = extreme_half_full_is_odd
     half_full_partial_is_odd = not extreme_half_full_is_odd
     half_empty_odd, half_empty_even = day21_reachable(
-        R, C, rocks, corner_r, corner_c, (R // 2)-1)
+        R, C, rocks, corner_r, corner_c, (R // 2) - 1)
     half_full_odd, half_full_even = day21_reachable(
-        R, C, rocks, corner_r, corner_c, (R-1) + (R // 2))
+        R, C, rocks, corner_r, corner_c, (R - 1) + (R // 2))
     half_full = half_full_odd if half_full_partial_is_odd else half_full_even
     half_empty = half_empty_odd if half_empty_partial_is_odd else half_empty_even
 
@@ -150,137 +127,84 @@ def day21_total(R, C, rocks, st, mid_r, mid_c, corner_r, corner_c):
         full_diag_even * len(corner_even) + \
         full_diag_odd * len(corner_odd) + \
         half_full_partial * len(half_full) + \
-        half_empty_partial * len(half_empty)  # lonely extreme
+        half_empty_partial * len(half_empty)
 
 def day21_2(data: list[str]):
     x = day21_parse(data)
 
-#     Right and right donw
-# down and left down
-# left and up left
-# up and top right
-
     grid = x
     R = len(grid)
     C = len(grid[0])
-    rocks = set()
-    start = (-1, -1)
-    for r in range(R):
-        for c in range(C):
-            if grid[r][c] == "#":
-                rocks.add((r, c))
-            elif grid[r][c] == "S":
-                start = (r, c)
-            else:
-                assert grid[r][c] == "."
     st = 26501365
-    # print(R)
+    _, rocks = day21_info(grid)
     assert R == C
-    #R, st, rocks = 7, 31, set()
-    # st = 31
-    # rocks = set()
-    # st = 10
-    # https://docs.google.com/spreadsheets/d/1St7OyhUKuvzdPOLTK7qejzADFwP-LLFuJOm5MUsJBQ4/edit?usp=sharing
+    # solution is hardcoded for:
+    # odd size
+    assert R % 2 == 1
+    # odd steps
+    assert st % 2 == 1
+    # even quocient of steps over R
+    assert (st // R) % 2 == 0
+    # odd remainder of steps over R
+    assert (st % R) % 2 == 1
+    # remainder of steps just shy of half size
+    assert (st % R) == R // 2
+    # limits are all free
+    # top
+    assert all(grid[0][c] == "." for c in range(C))
+    # right
+    assert all(grid[r][C - 1] == "." for r in range(R))
+    # bottom
+    assert all(grid[R - 1][c] == "." for c in range(C))
+    # left
+    assert all(grid[r][0] == "." for r in range(R))
+    # Lines crossing the middle also free
+    # vertical
+    assert all(grid[r][C // 2] in [".", "S"] for r in range(R))
+    # horizontal
+    assert all(grid[R // 2][c] in [".", "S"] for c in range(C))
+
+    # R, st, rocks = 7, 31, set()
+
+    # Diamond can be divided into 4 quadrants + the center piece
+    # since we can go straight to the edges and navigate to each
+    # corner of a 'sub-square'
+    #       2
+    #      122
+    #     11222
+    #    111C333
+    #     44433
+    #      443
+    #       4
+    #
+    # Number of odd positions reached on center square
     center_odd, _ = day21_reachable(R, R, rocks, R // 2, R // 2, R + R)
     center = len(center_odd)
-    right_down_right = day21_total(
-        R, R, rocks, st, R // 2, R - 1, R - 1, R - 1)
-    down_down_left = day21_total(R, R, rocks, st, R - 1, R // 2, R - 1, 0)
-    left_up_left = day21_total(R, R, rocks, st, R // 2, 0, 0, 0)
-    up_up_right = day21_total(R, R, rocks, st, 0, R // 2, 0, R - 1)
-    # aa,bb,cc,dd =
 
+    # Number of positions of top left quadrant
+    right_down_right = day21_quadrant(
+        R, R, rocks, st, R // 2, R - 1, R - 1, R - 1)
+
+    # Number of positions of top right quadrant
+    down_down_left = day21_quadrant(R, R, rocks, st, R - 1, R // 2, R - 1, 0)
+
+    # Number of positions of bottom right quadrant
+    left_up_left = day21_quadrant(R, R, rocks, st, R // 2, 0, 0, 0)
+
+    # Number of positions of bottom left quadrant
+    up_up_right = day21_quadrant(R, R, rocks, st, 0, R // 2, 0, R - 1)
+
+    # too low:
     #           614455686112700
     #           614455686112704
     #           614455686113074
-    # too low:  614455686113224
+    #           614455686113224
 
-    #           622926941971282
-    
-    # too high: 631398149479603
+    # too high:
+    #           631398149479603
     return center + right_down_right + down_down_left + left_up_left + up_up_right
 
-    d = defaultdict(lambda: defaultdict(list))
 
-    assert all(r != start[0] for r, c in rocks)
-    assert all(c != start[1] for r, c in rocks)
-    assert start == (R // 2, C // 2)
-    assert R == C
-    assert R % 2 == 1
-    # R = 131
-    print(start)
-
-    center_odd, _ = day21_reachable(R, C, rocks, start[0], start[1], R + C)
-    top_left_odd, _ = day21_reachable(R, C, rocks, 0, 0, R + C)
-    top_right_odd, _ = day21_reachable(R, C, rocks, 0, C - 1, R + C)
-    bottom_left_odd, _ = day21_reachable(R, C, rocks, R - 1, 0, R + C)
-    bottom_right_odd, _ = day21_reachable(R, C, rocks, R - 1, C - 1, R + C)
-
-    _, mid_top_even = day21_reachable(R, C, rocks, 0, C // 2, R + C)
-    _, mid_bottom_even = day21_reachable(R, C, rocks, R - 1, C // 2, R + C)
-    _, mid_left_even = day21_reachable(R, C, rocks, R // 2, 0, R + C)
-    _, mid_right_even = day21_reachable(R, C, rocks, R // 2, C - 1, R + C)
-
-    # reacheable_odd -> center
-    # left_side
-    max_len = st // R
-    full_lines = max_len - 1  # remove center
-    x = (max_len + 1) * (max_len) // 2
-    full_diags = x - full_lines - 1
-
-    half_empty = full_lines + 1  # R//2 moves left corner
-    half_full = half_empty - 1  # R moves left corner
-    extremes = 4
-    extremes_half_full = 4  # R moves left sides
-    # print(full_diags, full_lines)
-    # print(top_left_odd, top_right_odd, bottom_left_odd, bottom_right_odd, mid_top_even, mid_bottom_even, mid_left_even, mid_right_even)
-    return len(center_odd) + \
-        full_lines * len(mid_bottom_even) + \
-        full_lines * len(mid_top_even) + \
-        full_lines * len(mid_left_even) + \
-        full_lines * len(mid_right_even) + \
-        full_diags * len(top_left_odd) + \
-        full_diags * len(top_right_odd) + \
-        full_diags * len(bottom_left_odd) + \
-        full_diags * len(bottom_right_odd) + \
-        half_empty * len([1 for (_, _), l in top_left_odd if l <= R // 2]) + \
-        half_empty * len([1 for (_, _), l in top_right_odd if l <= R // 2]) + \
-        half_empty * len([1 for (_, _), l in bottom_left_odd if l <= R // 2]) + \
-        half_empty * len([1 for (_, _), l in bottom_right_odd if l <= R // 2]) + \
-        half_full * len([1 for (_, _), l in top_left_odd if l <= R // 2 + R]) + \
-        half_full * len([1 for (_, _), l in top_right_odd if l <= R // 2 + R]) + \
-        half_full * len([1 for (_, _), l in bottom_left_odd if l <= R // 2 + R]) + \
-        half_full * len([1 for (_, _), l in bottom_right_odd if l <= R // 2 + R]) + \
-        extremes
-    # extremes_half_full * len([1 for (_,_), l in  if l <= R]) + \
-
-    # for rrr in [start[0]]:#range(R):
-    #     for ccc in [start[1]]:#range(C):
-    #         if (rrr, ccc) in rocks:
-    #             continue
-    #         # if rrr != 0 and ccc != 0 and rrr != R - 1 and ccc != C - 1:
-    #         #     continue
-    #         start = (rrr, ccc)
-    #         q: deque[tuple[tuple[int, int], int]] = deque([(start, 0)])
-    #         seen = set()
-    #         while q:
-    #             (r, c), l = q.popleft()
-    #             if (r, c) in seen:
-    #                 continue
-    #             seen.add((r, c))
-    #             d[start][(r, c)].append(l)
-
-    #             D = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-    #             for dr, dc in D:
-    #                 rr, cc = (r + dr), (c + dc)
-
-    #                 if 0 <= rr < R and 0 <= cc < C and (rr, cc) not in rocks:
-    #                     q.append(((rr, cc), l + 1))
-    # return len(d)
-
-
-    # R//2
-    # return len(day20_dp(start[0], start[1], st, R, C, rocks, {}))
 """ MAIN FUNCTION """
 
 if __name__ == "__main__":
