56fd24d925ec453f4f65a26db23055db6a5ff7fa
Hélder Marques <helder.marques@outsystems.com>
2024-12-26 23:29:13 +0000

    Tweak day 11 2016

--- 2016/day11.py
+++ 2016/day11.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-from collections import defaultdict, deque
+from collections import deque
 from copy import deepcopy
 import functools
-from heapq import heappop, heappush
 import os
 import sys
 
@@ -19,66 +18,44 @@ EXPECTED_1 = 11
 EXPECTED_2 = None
 
 def day11_parse(data: list[str]):
-    floors = [[] for _ in range(4)]
+    floors: list[list[tuple[int, bool]]] = [[] for _ in range(4)]
+    types = {}
+    counter = 0
     for i, line in enumerate(data):
         # The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
         # The fourth floor contains nothing relevant.
         parts = line.split("contains ")[1].split(", ")
         if "nothing relevant" in parts[0]:
             continue
-        floors[i].extend(part.strip("and a").strip(".").replace("-compatible", "")
-                         for part in parts)
+
+        for part in parts:
+            typ, kind = part.strip("and a").strip(
+                ".").replace("-compatible", "").split(" ")
+            if typ not in types:
+                types[typ] = counter
+                counter += 1
+            floors[i].append((types[typ], kind == "generator"))
 
     return floors
 
-def day11_can_leave(obj: str, floor: int, floors: list[list[str]]):
-    if obj.endswith("microchip"):
-        return True
-    assert obj.endswith("generator")
-    typ = obj.split(" ")[0]
-    return not f"{typ} microchip" in floors[floor] or \
-        all(obj2 == obj or obj2.endswith("microchip")
-            for obj2 in floors[floor])
-
-def day11_can_enter(obj: str, floor: list[str]):
+@functools.cache
+def day11_can_enter(obj: tuple[str, str], floor: tuple[tuple[int, bool]]):
+    typ, is_generator = obj
     # can't enter if it would fry another chip
-    if obj.endswith("generator"):
-        typ = obj.split(" ")[0]
-        for obj2 in floor:
-            obj2_typ = obj2.split(" ")[0]
-            if obj2.endswith("microchip") and \
+    if is_generator:
+        for obj2_typ, obj2_is_generator in floor:
+            if not obj2_is_generator and \
                obj2_typ != typ and \
-               f"{obj2_typ} generator" not in floor:
+               (obj2_typ, True) not in floor:
                 return False
         return True
-    assert obj.endswith("microchip")
-    typ = obj.split(" ")[0]
-    for obj2 in floor:
-        obj2_typ = obj2.split(" ")[0]
-        if obj2.endswith("generator") and \
+    assert not is_generator
+    for obj2_typ, obj2_is_generator in floor:
+        if obj2_is_generator and \
            obj2_typ != typ and \
-           f"{typ} generator" not in floor:
+           (typ, True) not in floor:
             return False
     return True
-
-def day11_compatible(obj1: str, obj2: str):
-
-    return obj1.endswith("generator") or obj2.endswith("generator")
-
-def day11_same_type(obj1: str, obj2: str):
-    typ1 = obj1.split(" ")[0]
-    typ2 = obj2.split(" ")[0]
-    if typ1 == typ2:
-        assert obj1.endswith("generator") and obj2.endswith("microchip") or \
-            obj2.endswith("generator") and obj1.endswith("microchip")
-    return typ1 == typ2
-
-def day11_cost(floors):
-    # return 0
-    ans = 0
-    for i, objs in enumerate(floors):
-        ans += (3 - i) * len(objs)
-    return ans
 
 def day11_validate(floor):
     return day11_validate_aux(tuple(floor))
@@ -89,40 +66,23 @@ def day11_validate_aux(floor):
         if not day11_can_enter(obj1, floor):
             return False
     return True
-    # if obj1.endswith("generator"):
-    #     continue
-    # for obj2 in objs:
-    #     if obj1 == obj2:
-    #         continue
-
-    #     if day11_same_type(obj1, obj2):
-    #         continue
-
-    #     if obj1.endswith("generator"):
-    #         continue
-
-    #     typ1 = obj1.split(" ")[0]
-    #     typ2 = obj2.split(" ")[0]
-    #     if not (f"{typ1} generator" in objs or
-    #             f"{typ2} generator" in objs):
-    #         assert False
 
 def day11_solve(data, part2):
     data = day11_parse(data)
     floors = data
     if part2:
-        floors[0].extend(["elerium generator", "elerium microchip",
-                          "dilithium generator", "dilithium microchip"])
+        max_typ = max(max(typ for typ, _ in floor)
+                      for floor in floors if len(floor) > 0)
+        floors[0] += [(max_typ + 1, True), (max_typ + 1, False),
+                      (max_typ + 2, True), (max_typ + 2, False)]
         assert day11_validate(floors[0]) and day11_validate(floors[1]) and \
             day11_validate(floors[2]) and day11_validate(floors[3])
     total = sum(len(floor) for floor in floors)
-    # q: deque[tuple[int, int, int, list[list[str]]]
-    #         ] = deque([(day11_cost(floors), 0, 0, deepcopy(floors))])
-    q: list[tuple[int, int, int, list[list[str]]]
-            ] = [(day11_cost(floors), 0, 0, deepcopy(floors))]
+    q: deque[tuple[int, int, list[list[tuple[int, bool]]]]] = \
+        deque([(0, 0, deepcopy(floors))])
     seen = set()
     while q:
-        h, cost, e, floors = heappop(q)
+        cost, e, floors = q.popleft()
         if len(floors[-1]) == total:
             return cost
 
@@ -134,43 +94,31 @@ def day11_solve(data, part2):
         for delta in [-1, 1]:
             new_e = e + delta
             if 0 <= new_e < 4:
-                # el = (day11_cost(floors), cost +
-                #       1, new_e, deepcopy(floors))
-                # heappush(q, el)
                 for i, obj1 in enumerate(floors[e]):
-                    # if day11_can_leave(obj1, e, floors) and day11_can_enter(obj1, new_e, floors):
-                    new_floors = deepcopy(floors)
+                    new_floors = [list(floor)
+                                  for floor in floors]
                     new_floors[new_e].append(obj1)
                     new_floors[e].remove(obj1)
                     if day11_validate(new_floors[new_e]) and day11_validate(new_floors[e]):
-                        el = (h - delta + 1,
-                              cost + 1, new_e, new_floors)
-                        heappush(q, el)
-                        # q.append(el)
-                    for j, obj2 in enumerate(floors[e]):
-                        if j <= i:
-                            continue
-                        # if not day11_compatible(obj1, obj2):
-                        #     continue
+                        el = (cost + 1, new_e, new_floors)
+                        q.append(el)
+                    j = i + 1
+                    while j < len(floors[e]):
+                        obj2 = floors[e][j]
+                        j += 1
 
                         assert obj1 != obj2
 
-                        new_floors = deepcopy(floors)
+                        new_floors = [list(floor)
+                                      for floor in floors]
                         new_floors[new_e].append(obj1)
                         new_floors[new_e].append(obj2)
                         new_floors[e].remove(obj1)
                         new_floors[e].remove(obj2)
                         if not day11_validate(new_floors[new_e]) or not day11_validate(new_floors[e]):
                             continue
-                        el = (h - 2 * delta + 1,
-                              cost + 1, new_e, new_floors)
-                        heappush(q, el)
-                        # q.append(el)
-
-                        # if day11_same_type(obj1, obj2) or \
-                        #     (day11_can_leave(obj1, e, floors) and day11_can_enter(obj1, new_e, floors) and
-                        #      day11_can_leave(obj2, e, floors) and day11_can_enter(obj2, new_e, floors)):
-
+                        el = (cost + 1, new_e, new_floors)
+                        q.append(el)
     assert False
 
 def day11_1(data):
