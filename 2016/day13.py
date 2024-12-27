0f8baaf77a3674a724a545de9de4a618fe230fe0
Hélder Marques <helder.marques@outsystems.com>
2024-12-27 07:35:10 +0000

    Add solution day 13 2016

--- 2016/day13.py
+++ 2016/day13.py
@@ -9,7 +9,7 @@ FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, FILE_DIR + "/")
 sys.path.insert(0, FILE_DIR + "/../")
 # pylint: disable=wrong-import-position
-from common.utils import main  # NOQA: E402
+from common.utils import BLUE_CIRCLE, WHITE_SQUARE, main  # NOQA: E402
 from common.utils import day_with_validation  # NOQA: E402
 
 YEAR = 2016
@@ -30,6 +30,23 @@ def day13_wall(r, c, fav):
         value = value >> 1
     return cnt % 2 == 1
 
+def day13_print(seen, fav):
+    R = max(r for r, _ in seen)
+    C = max(c for r, c in seen)
+    for r in range(R):
+        line = ""
+        for c in range(C):
+            if (r, c) in seen:
+                assert not day13_wall(r, c, fav)
+                line += BLUE_CIRCLE
+            elif day13_wall(r, c, fav):
+                assert (r, c) not in seen
+                line += WHITE_SQUARE
+            else:
+                line += " "
+        print(line)
+    print()
+
 def day13_solve(data, part2):
     data = day13_parse(data)
     fav_number = data
@@ -41,7 +58,6 @@ def day13_solve(data, part2):
     r, c = 1, 1
     q: deque[tuple[int, int, int]] = deque([(0, 1, 1)])
     seen = set()
-    p2 = set()
     while q:
         cost, r, c = q.popleft()
         if (r, c) in seen:
@@ -52,26 +68,22 @@ def day13_solve(data, part2):
             return cost
 
         if part2:
-            if cost <= 50:
-                print(len(seen))
-                p2.add((r, c))
             if cost == 50:
                 continue
 
         for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             rr, cc = r + dr, c + dc
-            if rr < 0 or cc < 0 or day13_wall(r, c, fav_number):
+            if rr < 0 or cc < 0 or day13_wall(rr, cc, fav_number):
                 continue
 
             q.append((cost + 1, rr, cc))
 
-    # high 291
-    # high 283
-    return len(p2)
+    # day13_print(p2, fav_number)
+    return len(seen)
 
 def day13_1(data):
-    # wrong 23
-    # wrong 21
+    # high 291
+    # high 283
     return day13_solve(data, False)
 
 def day13_2(data):
